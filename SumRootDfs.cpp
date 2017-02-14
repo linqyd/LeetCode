1f7a34b02b847216720c2b429c8ba50a8269aebd
Sylvia-LIN <linqyd@live.com>
2017-02-14 17:44:26 +0800

    little change

---
+++ SumRootDfs.cpp
@@ -0,0 +1,23 @@
+#include <stddef.h>
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+ 
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+       
+    }
+
+    
+};
+
+int main(int argc, char *argv[])
+ 	{
+ 		
+    }
+
